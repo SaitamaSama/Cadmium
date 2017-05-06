ea34c2aff1c1e27a99b47e342dec15ff4c706fc3
Gourab Nag <gourabnag12@gmail.com>
2017-05-06 21:13:51 +0530

    do things

--- public/Scripts/MaterialScript.js
+++ public/Scripts/MaterialScript.js
@@ -1,67 +1,82 @@
 ;(() => {
     "use strict";
     // Get Started Page Feature Slider
-    let featureSlider = document.querySelector('#started-feature-slider');
-    let features = Array.from(document.querySelectorAll('#started-feature-slider .feature'));
+    /* let featureSlider = document.querySelector('#started-feature-slider');
+     let features = Array.from(document.querySelectorAll('#started-feature-slider .feature'));
 
-    let featureLeftMover = document.querySelector('#started-left-feature-mover');
-    let featureRightMover = document.querySelector('#started-right-feature-mover');
+     let featureLeftMover = document.querySelector('#started-left-feature-mover');
+     let featureRightMover = document.querySelector('#started-right-feature-mover');
 
-    featureLeftMover.addEventListener('click', () => {
-        let activeFeature = document.querySelector('#started-feature-slider .feature.active');
+     featureLeftMover.addEventListener('click', () => {
+     let activeFeature = document.querySelector('#started-feature-slider .feature.active');
 
-        if(features.indexOf(activeFeature) == 0) {
-            // Go back to last
-            activeFeature.classList.remove('active');
-            features[features.length - 1].classList.add('active');
-            return;
+     if(features.indexOf(activeFeature) == 0) {
+     // Go back to last
+     activeFeature.classList.remove('active');
+     features[features.length - 1].classList.add('active');
+     return;
+     }
+
+     activeFeature.classList.remove('active');
+     features[features.indexOf(activeFeature) - 1].classList.add('active');
+     });
+
+     featureRightMover.addEventListener('click', () => {
+     let activeFeature = document.querySelector('#started-feature-slider .feature.active');
+
+     if(features.indexOf(activeFeature) == features.length - 1) {
+     // Go back to first
+     activeFeature.classList.remove('active');
+     features[0].classList.add('active');
+     return;
+     }
+
+     activeFeature.classList.remove('active');
+     features[features.indexOf(activeFeature) + 1].classList.add('active');
+     });
+
+     // Moving features in interval of 5000ms (5seconds)
+     setInterval(() => {
+     let activeFeature = document.querySelector('#started-feature-slider .feature.active');
+
+     if(features.indexOf(activeFeature) <= features.length - 2) {
+     activeFeature.classList.remove('active');
+     features[features.indexOf(activeFeature) + 1].classList.add('active');
+     }
+     if(features.indexOf(activeFeature) == features.length - 1) {
+     activeFeature.classList.remove('active');
+     features[0].classList.add('active');
+     }
+     }, 5000);
+
+     // Scroll to bottom on bottom button click
+     document.querySelector('#started-header-to-main').addEventListener('click', () => {
+     let scrollTopped = 0;
+     let scrollTopInterval = setInterval(() => {
+     if(scrollTopped >= window.innerHeight / 2) {
+     scrollTopped = scrollTopped + 5;
+     } else {
+     scrollTopped = scrollTopped + 10;
+     }
+     document.body.scrollTop = scrollTopped;
+     if(scrollTopped >= window.innerHeight) {
+     clearInterval(scrollTopInterval);
+     }
+     }, 1);
+     });*/
+
+    let fbToggle = false;
+    document.querySelector('.brand-icon').addEventListener('click', () => {
+        if(!fbToggle) {
+            Array.from(document.querySelectorAll('.curved-container button')).forEach((button) => {
+                button.classList.add('opened');
+            });
+            fbToggle = true;
+        } else if (fbToggle) {
+            Array.from(document.querySelectorAll('.curved-container button')).forEach((button) => {
+                button.classList.remove('opened');
+            });
+            fbToggle = false;
         }
-
-        activeFeature.classList.remove('active');
-        features[features.indexOf(activeFeature) - 1].classList.add('active');
-    });
-
-    featureRightMover.addEventListener('click', () => {
-        let activeFeature = document.querySelector('#started-feature-slider .feature.active');
-
-        if(features.indexOf(activeFeature) == features.length - 1) {
-            // Go back to first
-            activeFeature.classList.remove('active');
-            features[0].classList.add('active');
-            return;
-        }
-
-        activeFeature.classList.remove('active');
-        features[features.indexOf(activeFeature) + 1].classList.add('active');
-    });
-
-    // Moving features in interval of 5000ms (5seconds)
-    setInterval(() => {
-        let activeFeature = document.querySelector('#started-feature-slider .feature.active');
-
-        if(features.indexOf(activeFeature) <= features.length - 2) {
-            activeFeature.classList.remove('active');
-            features[features.indexOf(activeFeature) + 1].classList.add('active');
-        }
-        if(features.indexOf(activeFeature) == features.length - 1) {
-            activeFeature.classList.remove('active');
-            features[0].classList.add('active');
-        }
-    }, 5000);
-
-    // Scroll to bottom on bottom button click
-    document.querySelector('#started-header-to-main').addEventListener('click', () => {
-        let scrollTopped = 0;
-        let scrollTopInterval = setInterval(() => {
-            if(scrollTopped >= window.innerHeight / 2) {
-                scrollTopped = scrollTopped + 5;
-            } else {
-                scrollTopped = scrollTopped + 10;
-            }
-            document.body.scrollTop = scrollTopped;
-            if(scrollTopped >= window.innerHeight) {
-                clearInterval(scrollTopInterval);
-            }
-        }, 1);
     });
 })();
