2240762c7951477e006f540679b6d27b627b7400
Gourab Nag <gourabnag12@gmail.com>
2017-05-07 17:22:17 +0530

    do things

--- public/Scripts/MaterialScript.js
+++ public/Scripts/MaterialScript.js
@@ -1,85 +1,25 @@
 ;(() => {
     "use strict";
-    // Get Started Page Feature Slider
-    /* let featureSlider = document.querySelector('#started-feature-slider');
-     let features = Array.from(document.querySelectorAll('#started-feature-slider .feature'));
-
-     let featureLeftMover = document.querySelector('#started-left-feature-mover');
-     let featureRightMover = document.querySelector('#started-right-feature-mover');
-
-     featureLeftMover.addEventListener('click', () => {
-     let activeFeature = document.querySelector('#started-feature-slider .feature.active');
-
-     if(features.indexOf(activeFeature) == 0) {
-     // Go back to last
-     activeFeature.classList.remove('active');
-     features[features.length - 1].classList.add('active');
-     return;
-     }
-
-     activeFeature.classList.remove('active');
-     features[features.indexOf(activeFeature) - 1].classList.add('active');
-     });
-
-     featureRightMover.addEventListener('click', () => {
-     let activeFeature = document.querySelector('#started-feature-slider .feature.active');
-
-     if(features.indexOf(activeFeature) == features.length - 1) {
-     // Go back to first
-     activeFeature.classList.remove('active');
-     features[0].classList.add('active');
-     return;
-     }
-
-     activeFeature.classList.remove('active');
-     features[features.indexOf(activeFeature) + 1].classList.add('active');
-     });
-
-     // Moving features in interval of 5000ms (5seconds)
-     setInterval(() => {
-     let activeFeature = document.querySelector('#started-feature-slider .feature.active');
-
-     if(features.indexOf(activeFeature) <= features.length - 2) {
-     activeFeature.classList.remove('active');
-     features[features.indexOf(activeFeature) + 1].classList.add('active');
-     }
-     if(features.indexOf(activeFeature) == features.length - 1) {
-     activeFeature.classList.remove('active');
-     features[0].classList.add('active');
-     }
-     }, 5000);
-
-     // Scroll to bottom on bottom button click
-     document.querySelector('#started-header-to-main').addEventListener('click', () => {
-     let scrollTopped = 0;
-     let scrollTopInterval = setInterval(() => {
-     if(scrollTopped >= window.innerHeight / 2) {
-     scrollTopped = scrollTopped + 5;
-     } else {
-     scrollTopped = scrollTopped + 10;
-     }
-     document.body.scrollTop = scrollTopped;
-     if(scrollTopped >= window.innerHeight) {
-     clearInterval(scrollTopInterval);
-     }
-     }, 1);
-     });*/
 
     let fbToggle = false;
     document.querySelector('.brand-icon').addEventListener('click', () => {
         if(!fbToggle) {
-            document.querySelector('.brand-icon').style.transform = 'rotate(360deg)';
+            //document.querySelector('.brand-icon').style.transform = 'rotate(360deg)';
+            document.querySelector('.brand-icon').classList.add('opened');
             setTimeout(() => {
-                componentHandler.updateElements(Array.from(document.querySelectorAll('.mdl-toolip')));
+                document.querySelector('.brand-icon').classList.remove('opened');
+                componentHandler.upgradeElements(Array.from(document.querySelectorAll('.mdl-toolip')));
             }, 501);
             Array.from(document.querySelectorAll('.curved-container button')).forEach((button) => {
                 button.classList.add('opened');
             });
             fbToggle = true;
         } else if (fbToggle) {
-            document.querySelector('.brand-icon').style.transform = 'rotate(0deg)';
+            //document.querySelector('.brand-icon').style.transform = 'rotate(0deg)';
+            document.querySelector('.brand-icon').classList.add('opened');
             setTimeout(() => {
-                componentHandler.updateElements(Array.from(document.querySelectorAll('.mdl-toolip')));
+                document.querySelector('.brand-icon').classList.remove('opened');
+                componentHandler.upgradeElements(Array.from(document.querySelectorAll('.mdl-toolip')));
             }, 501);
             Array.from(document.querySelectorAll('.curved-container button')).forEach((button) => {
                 button.classList.remove('opened');
@@ -87,4 +27,91 @@
             fbToggle = false;
         }
     });
+
+    class OTPer {
+        constructor(inputElement, dialogElement, snackbarElement) {
+            this.inputElement = inputElement;
+            this.dialogElement = dialogElement;
+            this.snackbarElement = snackbarElement;
+
+            inputElement.addEventListener('keypress', (e) => {
+                if(e.keyCode == 13) {
+                    this.handleEnter();
+                }
+            });
+
+            document.querySelector('#resend-otp').addEventListener('click', this.resend);
+            document.querySelector('#submit-otp').addEventListener('click', this.aunthenticate.bind(this));
+        }
+
+        handleEnter() {
+            if(this.inputElement.value.trim().length == 10) {
+                let mobileNo = this.inputElement.value.trim();
+                // API Request
+                // Open OTP dialog
+                this.dialogElement.showModal();
+            } else {
+                this.snackbarElement.MaterialSnackbar.showSnackbar({
+                    message: "Please provide a valid mobile number"
+                });
+            }
+        }
+
+        createNotification(background, color, message) {
+            let notification = document.createElement('div');
+            notification.classList.add('notify');
+            notification.style.background = background;
+            notification.style.color = color;
+            notification.innerHTML = message;
+
+            document.body.appendChild(notification);
+
+            setTimeout(() => {
+                notification.classList.add('appear');
+                setTimeout(() => {
+                    notification.classList.remove('appear');
+                }, 5000);
+            }, 201);
+        }
+
+        resend() {
+            document.querySelector('#enter-page').classList.remove('active');
+            document.querySelector('#resend-page').classList.add('active');
+            // Resend req
+            let presentColor = '';
+            let presentContent = '';
+            setTimeout(() => {
+                presentColor = document.querySelector('#resend-page').style.background;
+                presentContent = document.querySelector('#resend-page').innerHTML;
+                document.querySelector('#resend-page').innerHTML = '' +
+                    '<section class="text-center">' +
+                    '<i class="material-icons big-text-thin" style="color: #ffeb3b;">done</i>' +
+                    '<br>' +
+                    '<span class="big-text-thin">Sent</span>' +
+                    '</section>';
+                document.querySelector('#resend-page').style.background = '#00bcd4';
+            }, 7500);
+            setTimeout(() => {
+                document.querySelector('#resend-page').classList.remove('active');
+                document.querySelector('#resend-page').style.background = presentColor;
+                document.querySelector('#resend-page').innerHTML = presentContent;
+                document.querySelector('#enter-page').classList.add('active');
+            }, 15000);
+        }
+
+        aunthenticate() {
+            let otp = document.querySelector('#otp').value.trim();
+            if(otp.length == 0) {
+                this.snackbarElement.MaterialSnackbar.showSnackbar({
+                    message: "Please provide a valid OTP"
+                });
+                return;
+            }
+
+            document.querySelector('#enter-page').classList.remove('active');
+            document.querySelector('#authentication-page').classList.add('active');
+        }
+    }
+
+    window.OTPer = OTPer;
 })();
